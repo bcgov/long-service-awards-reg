a5ec84f9f2116fb92bf309c7767cb32817be606a
andriitaran-psa <andrii.taran@gov.bc.ca>
2024-02-20 09:09:45 -0800

    added office phone to registration confirmation page

--- src/views/lsa/LSAConfirmation.jsx
+++ src/views/lsa/LSAConfirmation.jsx
@@ -7,14 +7,14 @@
 
 import FormStep from "@/components/common/FormStep";
 import formServices from "@/services/settings.services.js";
-import React, {useContext} from "react";
-import {LoadingContext, RegistrationContext} from "@/AppContext.js";
+import React, { useContext } from "react";
+import { LoadingContext, RegistrationContext } from "@/AppContext.js";
 import ConfirmationInput from "@/components/fieldsets/ConfirmationInput";
-import {BlockUI} from "primereact/blockui";
-import {Button} from "primereact/button";
-import {useNavigate} from "react-router-dom";
+import { BlockUI } from "primereact/blockui";
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 import MilestoneData from "@/views/lsa/data/MilestoneData";
-import {Panel} from "primereact/panel";
+import { Panel } from "primereact/panel";
 import FormData from "@/components/common/FormData.jsx";
 import ProfileData from "@/views/lsa/data/ProfileData.jsx";
 import ContactData from "@/views/lsa/data/ContactData";
@@ -28,42 +28,61 @@ import InfoLSADeclaration from "@/components/info/InfoLSADeclaration.jsx";
  */
 
 export default function LSAConfirmation() {
-
   const { loading } = useContext(LoadingContext);
   const { completed } = useContext(RegistrationContext);
   const navigate = useNavigate();
   const { registration } = useContext(RegistrationContext);
-  const {service} = registration || {};
+  const { service } = registration || {};
   const { previous_registration, cycle } = service || {};
 
   // get form step schema / default values
-  const previous = formServices.copy('lsa', 'supervisor');
-  const current = formServices.copy('lsa', 'confirmation');
-  const steps = formServices.get('lsa');
+  const previous = formServices.copy("lsa", "supervisor");
+  const current = formServices.copy("lsa", "confirmation");
+  const steps = formServices.get("lsa");
 
   // block confirmation fieldset if form is incomplete
-  return <FormStep steps={steps} previous={previous} current={current}>
-    {
-      completed && <Panel className={'mb-3'} header={'Confirm Registration Details'}>
-        <FormData form={'lsa'} id={'milestone'}><MilestoneData/></FormData>
-        <FormData form={'lsa'} id={'profile'}><ProfileData/></FormData>
-        <FormData form={'lsa'} id={'contact'}><ContactData/></FormData>
-          {
-            previous_registration
-                ? <Panel className={'mb-3'} header={'Award'}>Award Previously Selected</Panel>
-                : <FormData form={'lsa'} id={'awards'}><AwardData/></FormData>
-          }
-        <FormData form={'lsa'} id={'supervisor'}><SupervisorData/></FormData>
-      </Panel>
-    }
-    <BlockUI blocked={!completed && !loading} template={
-      <Button
-          onClick={()=>{navigate('/lsa/milestone')}}
-          label={'Click to Complete Your Registration'}
-      />}>
-      <ConfirmationInput>
-        <InfoLSADeclaration year={cycle} />
-      </ConfirmationInput>
-    </BlockUI>
-  </FormStep>;
+  return (
+    <FormStep steps={steps} previous={previous} current={current}>
+      {completed && (
+        <Panel className={"mb-3"} header={"Confirm Registration Details"}>
+          <FormData form={"lsa"} id={"milestone"}>
+            <MilestoneData />
+          </FormData>
+          <FormData form={"lsa"} id={"profile"}>
+            <ProfileData />
+          </FormData>
+          <FormData form={"lsa"} id={"contact"}>
+            <ContactData />
+          </FormData>
+          {previous_registration ? (
+            <Panel className={"mb-3"} header={"Award"}>
+              Award Previously Selected
+            </Panel>
+          ) : (
+            <FormData form={"lsa"} id={"awards"}>
+              <AwardData />
+            </FormData>
+          )}
+          <FormData form={"lsa"} id={"supervisor"}>
+            <SupervisorData />
+          </FormData>
+        </Panel>
+      )}
+      <BlockUI
+        blocked={!completed && !loading}
+        template={
+          <Button
+            onClick={() => {
+              navigate("/lsa/milestone");
+            }}
+            label={"Click to Complete Your Registration"}
+          />
+        }
+      >
+        <ConfirmationInput>
+          <InfoLSADeclaration year={cycle} />
+        </ConfirmationInput>
+      </BlockUI>
+    </FormStep>
+  );
 }
--- src/views/lsa/data/ContactData.jsx
+++ src/views/lsa/data/ContactData.jsx
@@ -5,76 +5,102 @@
  * MIT Licensed
  */
 
-import {useContext} from "react";
-import {RegistrationContext} from "@/AppContext";
-import {Fieldset} from "primereact/fieldset";
+import { useContext } from "react";
+import { RegistrationContext } from "@/AppContext";
+import { Fieldset } from "primereact/fieldset";
 
 /**
  * Recipient Profile Details
  */
 
 export default function ContactData() {
+  const { registration } = useContext(RegistrationContext);
+  const { contact } = registration || {};
+  const { personal_phone, office_address, personal_address, office_phone } =
+    contact || {};
 
-    const { registration } = useContext(RegistrationContext);
-    const {contact} = registration || {};
-    const { personal_phone, office_address, personal_address } = contact || {}
-
-    return <div className={'container'}>
-        <div className={'grid'}>
-            <div className={'col-6'}>Personal Phone Number</div>
-            <div className={'col-6'}>{personal_phone}</div>
-            <div className={'col-12'}>
-            <Fieldset legend={'Home Mailing Address'} toggleable>
-                <div className={'container'}>
-                    <div className={'grid'}>
-                {
-                    personal_address && personal_address.pobox
-                    && <><div className={'col-6'}>P.O. Box</div>
-                        <div className={'col-6'}>{personal_address && personal_address.pobox}</div></>
-                }
-                <div className={'col-6'}>Street Address</div>
-                <div className={'col-6'}>
-                    <div>{personal_address && personal_address.street1}</div>
-                    <div>{personal_address && personal_address.street2}</div>
-                </div>
-                <div className={'col-6'}>Community or City</div>
-                <div className={'col-6'}>{personal_address && personal_address.community}</div>
-                <div className={'col-6'}>Province</div>
-                <div className={'col-6'}>{personal_address && personal_address.province}</div>
-                <div className={'col-6'}>Country</div>
-                <div className={'col-6'}>{personal_address && personal_address.country}</div>
-                <div className={'col-6'}>Postal Code</div>
-                <div className={'col-6'}>{personal_address && personal_address.postal_code}</div>
+  return (
+    <div className={"container"}>
+      <div className={"grid"}>
+        <div className={"col-6"}>Personal Phone Number</div>
+        <div className={"col-6"}>{personal_phone}</div>
+        <div className={"col-12"}>
+          <Fieldset legend={"Home Mailing Address"} toggleable>
+            <div className={"container"}>
+              <div className={"grid"}>
+                {personal_address && personal_address.pobox && (
+                  <>
+                    <div className={"col-6"}>P.O. Box</div>
+                    <div className={"col-6"}>
+                      {personal_address && personal_address.pobox}
                     </div>
+                  </>
+                )}
+                <div className={"col-6"}>Street Address</div>
+                <div className={"col-6"}>
+                  <div>{personal_address && personal_address.street1}</div>
+                  <div>{personal_address && personal_address.street2}</div>
                 </div>
-            </Fieldset>
+                <div className={"col-6"}>Community or City</div>
+                <div className={"col-6"}>
+                  {personal_address && personal_address.community}
+                </div>
+                <div className={"col-6"}>Province</div>
+                <div className={"col-6"}>
+                  {personal_address && personal_address.province}
+                </div>
+                <div className={"col-6"}>Country</div>
+                <div className={"col-6"}>
+                  {personal_address && personal_address.country}
+                </div>
+                <div className={"col-6"}>Postal Code</div>
+                <div className={"col-6"}>
+                  {personal_address && personal_address.postal_code}
+                </div>
+              </div>
             </div>
-                <div className={'col-12'}>
-            <Fieldset legend={'Office Address'} toggleable>
-                <div className={'container'}>
-                    <div className={'grid'}>
-                {
-                    office_address && office_address.pobox
-                    && <><div className={'col-6'}>P.O. Box</div>
-                        <div className={'col-6'}>{office_address && office_address.pobox}</div></>
-                }
-                <div className={'col-6'}>Street Address</div>
-                <div className={'col-6'}>
-                    <div>{office_address && office_address.street1}</div>
-                    <div>{office_address && office_address.street2}</div>
-                </div>
-                <div className={'col-6'}>Community or City</div>
-                <div className={'col-6'}>{office_address && office_address.community}</div>
-                <div className={'col-6'}>Province</div>
-                <div className={'col-6'}>{office_address && office_address.province}</div>
-                <div className={'col-6'}>Country</div>
-                <div className={'col-6'}>{office_address && office_address.country}</div>
-                <div className={'col-6'}>Postal Code</div>
-                <div className={'col-6'}>{office_address && office_address.postal_code}</div>
-                    </div>
-                </div>
-            </Fieldset>
-                </div>
+          </Fieldset>
         </div>
+        <div className={"col-12"}>
+          <Fieldset legend={"Office Address"} toggleable>
+            <div className={"container"}>
+              <div className={"grid"}>
+                {office_address && office_address.pobox && (
+                  <>
+                    <div className={"col-6"}>P.O. Box</div>
+                    <div className={"col-6"}>
+                      {office_address && office_address.pobox}
+                    </div>
+                  </>
+                )}
+                <div className={"col-6"}>Street Address</div>
+                <div className={"col-6"}>
+                  <div>{office_address && office_address.street1}</div>
+                  <div>{office_address && office_address.street2}</div>
+                </div>
+                <div className={"col-6"}>Community or City</div>
+                <div className={"col-6"}>
+                  {office_address && office_address.community}
+                </div>
+                <div className={"col-6"}>Province</div>
+                <div className={"col-6"}>
+                  {office_address && office_address.province}
+                </div>
+                <div className={"col-6"}>Country</div>
+                <div className={"col-6"}>
+                  {office_address && office_address.country}
+                </div>
+                <div className={"col-6"}>Postal Code</div>
+                <div className={"col-6"}>
+                  {office_address && office_address.postal_code}
+                </div>
+                <div className={"col-6"}>Office Phone</div>
+                <div className={"col-6"}>{office_phone && office_phone}</div>
+              </div>
+            </div>
+          </Fieldset>
+        </div>
+      </div>
     </div>
+  );
 }
