849d2fcdd2266ab2b0a661e089154bfbc58b28aa
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-03-08 11:04:01 -0800

    Remove office address from summary when org uses bulk ship

--- src/views/lsa/data/SupervisorData.jsx
+++ src/views/lsa/data/SupervisorData.jsx
@@ -17,6 +17,7 @@ import { Fieldset } from "primereact/fieldset";
 export default function SupervisorData() {
   const { registration } = useContext(RegistrationContext);
   const { supervisor } = registration || {};
+  const { organization } = registration || {};
   const { first_name, last_name, office_email, office_address } =
     supervisor || {};
 
@@ -29,43 +30,45 @@ export default function SupervisorData() {
         <div className={"col-6"}>{last_name}</div>
         <div className={"col-6"}>Government Email Address</div>
         <div className={"col-6"}>{office_email}</div>
-        <div className={"col-12"}>
-          <Fieldset legend={"Office Address"} toggleable>
-            <div className={"container"}>
-              <div className={"grid"}>
-                {office_address && office_address.pobox && (
-                  <>
-                    <div className={"col-6"}>P.O. Box</div>
-                    <div className={"col-6"}>
-                      {office_address && office_address.pobox}
-                    </div>
-                  </>
-                )}
-                <div className={"col-6"}>Street Address</div>
-                <div className={"col-6"}>
-                  <div>{office_address && office_address.street1}</div>
-                  <div>{office_address && office_address.street2}</div>
-                </div>
-                <div className={"col-6"}>Community or City</div>
-                <div className={"col-6"}>
-                  {office_address && office_address.community}
-                </div>
-                <div className={"col-6"}>Province</div>
-                <div className={"col-6"}>
-                  {office_address && office_address.province}
-                </div>
-                <div className={"col-6"}>Country</div>
-                <div className={"col-6"}>
-                  {office_address && office_address.country}
-                </div>
-                <div className={"col-6"}>Postal Code</div>
-                <div className={"col-6"}>
-                  {office_address && office_address.postal_code}
+        {!organization.bulk && (
+          <div className={"col-12"}>
+            <Fieldset legend={"Office Address"} toggleable>
+              <div className={"container"}>
+                <div className={"grid"}>
+                  {office_address && office_address.pobox && (
+                    <>
+                      <div className={"col-6"}>P.O. Box</div>
+                      <div className={"col-6"}>
+                        {office_address && office_address.pobox}
+                      </div>
+                    </>
+                  )}
+                  <div className={"col-6"}>Street Address</div>
+                  <div className={"col-6"}>
+                    <div>{office_address && office_address.street1}</div>
+                    <div>{office_address && office_address.street2}</div>
+                  </div>
+                  <div className={"col-6"}>Community or City</div>
+                  <div className={"col-6"}>
+                    {office_address && office_address.community}
+                  </div>
+                  <div className={"col-6"}>Province</div>
+                  <div className={"col-6"}>
+                    {office_address && office_address.province}
+                  </div>
+                  <div className={"col-6"}>Country</div>
+                  <div className={"col-6"}>
+                    {office_address && office_address.country}
+                  </div>
+                  <div className={"col-6"}>Postal Code</div>
+                  <div className={"col-6"}>
+                    {office_address && office_address.postal_code}
+                  </div>
                 </div>
               </div>
-            </div>
-          </Fieldset>
-        </div>
+            </Fieldset>
+          </div>
+        )}
       </div>
     </div>
   );
