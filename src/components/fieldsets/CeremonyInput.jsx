f2cb083dc6220176a4e0d943beafdd940d014ce6
paulbothma-govbcca <paul.bothma@gov.bc.ca>
2024-10-25 13:53:23 -0700

    LSA-503 Changed buttons wording for ceremony and address

--- src/components/fieldsets/CeremonyInput.jsx
+++ src/components/fieldsets/CeremonyInput.jsx
@@ -7,7 +7,8 @@
 
 import {useFormContext, useWatch} from "react-hook-form";
 import {Panel} from "primereact/panel";
-import {SelectButton} from "primereact/selectbutton";
+import { Message } from 'primereact/message';
+import { RadioButton } from 'primereact/radiobutton';
 
 /**
  * Ceremony Attendance component.
@@ -24,28 +25,53 @@ export default function CeremonyInput() {
         name: "service.ceremony_opt_out",
     });
 
-    return <Panel className={'mb-3'} header={<>Ceremony Attendance</>}>
+    return <Panel className={'mb-3'} header={<>Ceremony Opt-Out</>}>
         <div className="container">
             <div className="grid">
                 <div className="col-12 form-field-container">
-                    <div className="flex align-items-center">
-                        <SelectButton
-                            className={'radio-toggle'}
-                            value={ceremonyOptOut ? 'Yes' : 'No'}
+                    <p>
+
+                        Do you want to opt-out of ceremony attendance, and receive your award only?
+                    </p>
+                    <div className="flex wrap">
+                        
+                        <RadioButton
+                            checked={ceremonyOptOut}
+                            inputId="ceremonyOptOutYes"
+                            name="ceremonyOptOut"
+                            value="Yes"
                             onChange={(e) => {
-                                setValue('service.ceremony_opt_out', e.value === 'Yes')
+                                setValue('service.ceremony_opt_out', e.value === "Yes")
                             }}
-                            options={['Yes', 'No']}
                         />
-                        <label className={'ml-3'}>
-                            Do you want to receive your award only and not attend the ceremony?
+                        <label className={'ml-3'} htmlFor="ceremonyOptOutYes">
+                            Yes, I would like to <span className={'font-bold'}>opt-out</span> of the ceremony
+                        </label>
+                    </div>
+                    
+                    <div className="flex wrap">
+                        
+                        <RadioButton
+                            checked={!ceremonyOptOut}
+                            inputId="ceremonyOptOutNo"
+                            name="ceremonyOptOut"
+                            value="No"
+                            onChange={(e) => {
+                                setValue('service.ceremony_opt_out', e.value === "Yes")
+                            }}
+                        />
+                        <label className={'ml-3'} htmlFor="ceremonyOptOutNo">
+                            No, I would like to attend the ceremony
                         </label>
                     </div>
                     <p>
-                        Selecting the award only option means you will not receive an invitation to a
+                        
+                        Selecting the <span className={'font-bold'}>opt-out</span> option means you will not receive an invitation to a
                         ceremony and you will receive your award following your organization’s ceremony
                         night in the fall.
+                        
                     </p>
+                    { false && ceremonyOptOut && <p><Message text="You have opted to not attend the ceremony as well." severity="info" /></p> }
                 </div>
             </div>
         </div>
